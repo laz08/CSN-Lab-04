79d287604cbff66bf5c5f31590d47bf8e462dd87
centeio <c.centeiojorge@outlook.pt>
2018-11-13 17:52:46 +0100

    task 1 finished

--- src/task_01.R
+++ src/task_01.R
@@ -50,38 +50,60 @@ communitiesNames = c("edge.betweenness",
                      "infomap")
 
 
-computeTrianglePartitionRatio <- function(graph, communityMethod){
-    
+computeMetrics <- function(graph, communityMethod){
+    m <- length(E(graph))
+    n <- length(V(graph))
+  
     numSubComm <- max(communityMethod$membership)
     verticesMembership <- communityMethod$membership
-    verticesPos <- seq(length(V(graph)))
-    res = c()
+    verticesPos <- seq(n)
+    degrees <- degree(graph)
+
+    
+    triangles = c()
+    conductances <- c()
+    modularities <- c()
+    expansions <- c()
     for(subGIdx in seq(numSubComm)){
         # Take the vertices on that subcommunity
         vOfSubComm <- verticesPos[verticesMembership == subGIdx]
         # Create subgraph of subcommunity subGIdx
         subG = induced_subgraph(graph, vids = vOfSubComm)
+        mc <- length(E(subG))
+        nc <- length(V(subG))
+        
+        #TRIANGLES
         tri = sum(count_triangles(subG)) # Sum of all the vertices triangles
-        metric = tri / numSubComm
-        res = append(res, metric) 
+        tpt = tri / numSubComm
+        weightedTPT = tpt * (nc/n)
+        triangles = append(triangles, weightedTPT) 
+        
+        #EXPANSION
+        subdegree <- degree(subG)
+        subsetOrgDegrees <- degrees[verticesMembership == subGIdx]
+        difdegree <- subsetOrgDegrees - subdegree
+        fc <- sum(difdegree)
+        expansion <- fc/numSubComm
+        weightedExpansion <- expansion * (nc/n)
+        expansions <- append(expansions, weightedExpansion)
+        
+        
+        #MODULARITY
+        p <- m / ((1/2) * n * (n - 1))
+        expected <- p*((1/2) * nc * (nc - 1))
+        modularity <- (1/(4*m))*(mc - expected)
+        weightedModularity <- modularity * (nc/n)
+        modularities <- append(modularities, weightedModularity)
+        
+        #CONDUCTANCE
+        conductance <- fc/(fc + 2*(mc))
+        weightedConductance <- conductance*(nc/n)
+        conductances <- append(conductances, weightedConductance)
     }
     
     # TODO: Check how to adapt metric for whole network
-    res <- sum(res)
+    res <- c(sum(triangles), sum(modularities), sum(conductances), sum(expansions))
     return(res)
-}
-
-computeExpansion <- function(graph){
-    return(0)
-}
-
-computeConductance <- function(graph){
-    return(0)
-}
-
-
-computeModularity <- function(graph){
-    return(0)
 }
 
 
@@ -89,7 +111,7 @@ computeTableForGraph <- function(graph){
 
     communities = computeDiffCommunities(graph)
     resTable <- data.table("Method" = character(),
-                           "TBT" = numeric(),
+                           "TPT" = numeric(),
                            "Expansion" = numeric(),
                            "Conductance" = numeric(),
                            "Modularity" = numeric(),
@@ -101,14 +123,16 @@ computeTableForGraph <- function(graph){
         c <- communities[i]; c <- c[[1]] # Dealing with the list messing up w/ the structure
         
         name <- communitiesNames[i]
-        TPT <- computeTrianglePartitionRatio(graph, c)
-        expansion <- computeExpansion(c)
-        conduct <- computeConductance(c)
-        mod <- computeModularity(c)
+        metrics <- computeMetrics(graph, c)
+        TPT <- metrics[1]
+        expansion <- metrics[4]
+        conduct <- metrics[3]
+        mod <- metrics[2]
         
         resTable <- rbind(resTable, list(name, TPT, expansion, conduct, mod))
     }
+    return (resTable)
 }
 
 graph = karate
-computeTableForGraph(karate)
+(t = computeTableForGraph(karate))
